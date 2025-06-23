ba887ae78f1742b55324e93e1bc2e6830fb91b02
Yair Morgenstern <yair_morgenstern@il.yair.morgenstern.m2max16>
2025-06-23 23:34:58 +0300

    Hopefully resolve build error in CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,11 @@
 
 includeBuild("gradle-plugin")
-includeBuild("compiler-plugin")
+includeBuild("compiler-plugin"){
+    dependencySubstitution { // for lib, even though it gets purity-compiler-plugin dependency via gradle plugin, will still use local compiler-plugin
+        substitute(module("io.github.yairm210:purity-compiler-plugin"))
+            .using(project(":"))
+    }
+}
 
 include(":lib")
 
