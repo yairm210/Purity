5664ed4da6643d2b901b92c810c4555577c51894
yairm210 <yairm210@hotmail.com>
2025-07-06 16:41:43 +0300

    0.0.10

--- compiler-plugin/build.gradle.kts
+++ compiler-plugin/build.gradle.kts
@@ -20,7 +20,7 @@ allprojects {
 }
 
 group = "io.github.yairm210"
-version = "0.0.9"
+version = "0.0.10"
 
 mavenPublishing {
     coordinates(group.toString(), "purity-compiler-plugin", version.toString())
--- gradle-plugin/src/main/kotlin/yairm210/purity/PurityGradlePlugin.kt
+++ gradle-plugin/src/main/kotlin/yairm210/purity/PurityGradlePlugin.kt
@@ -18,7 +18,7 @@ class PurityGradlePlugin : KotlinCompilerPluginSupportPlugin {
     companion object {
         const val COMPILER_PLUGIN_GROUP_NAME = "io.github.yairm210"
         const val ARTIFACT_NAME = "purity-compiler-plugin"
-        const val VERSION_NUMBER = "0.0.9"
+        const val VERSION_NUMBER = "0.0.10"
     }
 
     private var gradleExtension : PurityConfiguration = PurityConfiguration()
