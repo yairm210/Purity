cecc4b8da456c87196c34df9703fa54d671fb69c
yairm210 <yairm210@hotmail.com>
2025-07-06 16:05:44 +0300

    0.0.9

--- compiler-plugin/build.gradle.kts
+++ compiler-plugin/build.gradle.kts
@@ -20,7 +20,7 @@ allprojects {
 }
 
 group = "io.github.yairm210"
-version = "0.0.8"
+version = "0.0.9"
 
 mavenPublishing {
     coordinates(group.toString(), "purity-compiler-plugin", version.toString())
