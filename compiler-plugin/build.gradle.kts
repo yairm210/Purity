46a12bdf46048042434fe7a5123a2fe8e9fd2b11
Yair Morgenstern <yair_morgenstern@il.yair.morgenstern.m2max16>
2025-07-06 18:08:09 +0300

    0.0.12 Fix trying to read statements of synthetic body - whatever the hell that means

--- compiler-plugin/build.gradle.kts
+++ compiler-plugin/build.gradle.kts
@@ -20,7 +20,7 @@ allprojects {
 }
 
 group = "io.github.yairm210"
-version = "0.0.11"
+version = "0.0.12"
 
 mavenPublishing {
     coordinates(group.toString(), "purity-compiler-plugin", version.toString())
