1029902e1f600453c735d48a75d1a8887379adf0
yairm210 <yairm210@hotmail.com>
2025-07-10 18:59:14 +0300

    0.0.15 Fixed suppress, added more well known classes+functions We can now commit the sample since we can suppress bad example functions :)

--- compiler-plugin/build.gradle.kts
+++ compiler-plugin/build.gradle.kts
@@ -20,7 +20,7 @@ allprojects {
 }
 
 group = "io.github.yairm210"
-version = "0.0.14"
+version = "0.0.15"
 
 mavenPublishing {
     coordinates(group.toString(), "purity-compiler-plugin", version.toString())
