6292cb3831fed495c4b4f42706f17e012176e481
Yair Morgenstern <yair_morgenstern@il.yair.morgenstern.m2max16>
2025-07-13 00:19:34 +0300

    Added missing files

---
+++ annotations/src/commonMain/kotlin/Pure.kt
@@ -0,0 +1,10 @@
+package yairm210.purity.annotations
+
+import kotlin.annotation.AnnotationTarget.FUNCTION
+
+/**
+ * An annotation to indicate that this function is Pure.
+ * This means that it does not have any side effects and its output depends only on its input.
+ * Functions which read from mutable state are not pure.
+ */
+@Target(FUNCTION) public annotation class Pure
