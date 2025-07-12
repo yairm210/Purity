5da506156761b763e614b2249c76b1b7c1d9d033
Yair Morgenstern <yair_morgenstern@il.yair.morgenstern.m2max16>
2025-07-13 00:30:56 +0300

    0.0.17 sign publications...

--- gradle-plugin/src/main/kotlin/yairm210/purity/PurityGradlePlugin.kt
+++ gradle-plugin/src/main/kotlin/yairm210/purity/PurityGradlePlugin.kt
@@ -23,7 +23,7 @@ class PurityGradlePlugin : KotlinCompilerPluginSupportPlugin {
     companion object {
         const val COMPILER_PLUGIN_GROUP_NAME = "io.github.yairm210"
         const val ARTIFACT_NAME = "purity-compiler-plugin"
-        const val VERSION_NUMBER = "0.0.16"
+        const val VERSION_NUMBER = "0.0.17"
     }
 
     private var gradleExtension : PurityConfiguration = PurityConfiguration()
