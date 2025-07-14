e10fc7cfee100a93cd55945880c37da7d0200756
yairm210 <yairm210@hotmail.com>
2025-07-14 13:21:07 +0300

    0.0.19 Added more well-known pure functions

--- annotations/build.gradle.kts
+++ annotations/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "io.github.yairm210"
-version = "0.0.18"
+version = "0.0.19"
 
 kotlin {
     sourceSets{
--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
 
 
 group = "io.github.yairm210"
-version = "0.0.18"
+version = "0.0.19"
 
 gradlePlugin {
     website = "https://github.com/yairm210/purity"
