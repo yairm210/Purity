f3ae4ef9d199d152c9e93a651b19b7f3769115e6
yairm210 <yairm210@hotmail.com>
2025-07-20 17:34:58 +0300

    chore: Organized tests into test cases - what exactly we're testing where - and removed double tests

--- lib/src/jvmMain/kotlin/sample/JvmSampleTests.kt
+++ lib/src/jvmMain/kotlin/sample/JvmSampleTests.kt
@@ -16,173 +16,167 @@ enum class MyEnum {
     A, B
 }
 
-// 
+fun testWellKnownReadonlyFunctions(){
+    val map = HashMap<String, String>()
 
-val map = HashMap<String, String>()
-
-@Readonly
-fun correctReadonlyInterfaceOverrideUsage(): Boolean {
-    return map.containsKey("key")
-}
-
-//
-@Readonly
-fun correctSequenceChainingPure(): String {
-    return sequenceOf("Hello, ", "World!")
-        .joinToString(separator = "")
-}
-
-class A{
-    var x = 5
     @Readonly
-    fun getXX() = x
-}
-
-
-class B{
-    val x = 5
-    @Pure
-    fun getXX() = x
-}
-
-var enumMap = EnumMap<MyEnum, String>(MyEnum::class.java)
-
-@Readonly
-fun getUniques(uniqueType: MyEnum) = enumMap[uniqueType]
-    ?.asSequence()
-    ?: emptySequence()
-
-var myList = listOf("Hello", "World")
-@Readonly
-fun correctListChainingPure(): String {
-    return myList
-        .filter { it.isNotEmpty() }
-        .joinToString(separator = " ")
-}
-
-
-fun main() {
-    var external = 3
+    fun correctReadonlyInterfaceOverrideUsage(): Boolean {
+        return map.containsKey("key")
+    }
     
+    val enumMap = EnumMap<MyEnum, String>(MyEnum::class.java)
+
+    @Readonly
+    fun getUniques(uniqueType: MyEnum) = enumMap[uniqueType]
+        ?.asSequence()
+        ?: emptySequence()
+
+
+    @Readonly
+    fun sequenceChaining(): String {
+        return sequenceOf("Hello, ", "World!")
+            .joinToString(separator = "")
+    }
+}
+
+fun testClassVariableRetrieval(){
+    class A{
+        var x = 5
+        @Readonly
+        fun getXReadonly() = x
+        
+        @Pure @TestExpectCompileError
+        fun getXPure() = x
+    }
+    
+    class B {
+        val x = 5
+        @Pure
+        fun getXX() = x
+    }
+}
+
+fun testFunctionVariableGetSet(){
+    var functionVariable = 3
+
     // RIGHT: readonly, because reading external variables is allowed
     @Readonly
-    fun correctReadonly(): Int {
-        1.rangeTo(2)
-        val x = listOf(1,2)
-        x[0]
-        return external
-    }
-    
-    @Pure
-    fun give(a: Int): Int {
-        return a
+    fun readonlyGetVariable(): Int {
+        return functionVariable
     }
     
     @Pure @TestExpectCompileError
-    fun readsExternalVar(): Int {
-        return external
+    fun pureGetVariable(): Int {
+        return functionVariable
+    }
+    
+    @Readonly @TestExpectCompileError
+    fun readonlySetVariable() {
+        functionVariable = 4
     }
 
     @Pure @TestExpectCompileError
-    fun setsExternalVar(a: Int, b: Int): Int {
-        external = 4
-        return a
+    fun pureSetVariable() {
+        functionVariable = 4
+    }
+}
+
+fun testFunctionsCanOnlyCallTheirPurityAndHigher() {
+    fun unmarkedFunction(){}
+    @Readonly fun readonlyFunction(){}
+    @Pure fun pureFunction(){}
+    
+    @Readonly fun readonlyCorrect(){
+        readonlyFunction()
+        pureFunction() // Allowed, since pure is higher than readonly
     }
     
-    @Pure @TestExpectCompileError
-    fun callsReadonly(){
-        correctReadonly() // Pure functions cannot call readonly functions
+    @Readonly @TestExpectCompileError fun readonlyIncorrect(){
+        unmarkedFunction() // NOT allowed, since unmarked is lower than readonly
     }
-
-    @Pure
-    fun getList() = listOf(1,2)
     
-    @Readonly
-    fun callPureFunction() { // Readonly functions can call pure functions
-        getList().filter { it > 1 }
+    @Pure fun pureCorrect(){
+        pureFunction() // Allowed, since pure is equal to pure
+    }
+    
+    @Pure @TestExpectCompileError fun pureIncorrectReadonly(){
+        readonlyFunction() // NOT allowed, since readonly is lower than pure
     }
 
-    fun Int.self(): Int {
-        return this
+    @Pure @TestExpectCompileError fun pureIncorrectRUnmarked(){
+        unmarkedFunction() // NOT allowed, since readonly is lower than pure
     }
+}
 
-    // NOT reported as a problem since the variable is internal
-    @Pure
-    fun setsInternalVariable(): Int {
-        var internal = 3
-        internal = 4
-        val seq = sequence<String> {
-            // Subscope can alter its vars and function vars
-            var internal2 = 0
-            yield("Hello")
-            yield("World")
-            internal2 += 1
-            internal += 1
-        }
-        return internal
-    }
-
+fun testReadonlyFunctionsOnImmutableConsideredPure(){
     @Immutable
     val immutableList = arrayListOf(1,2,3)
-    
+
     @Pure
     fun readImmutable(index: Int): Int {
         immutableList.filter { it > 2 } // allowed as 'extensionReceiver is @Immutable'
         return immutableList[index] // allowed as 'dispatchReceiver is @Immutable'
     }
-    
+
     @Pure @TestExpectCompileError
     fun writeImmutable(){ // @Immutable only allows readonly functions to be considered pure, not write functions
         immutableList.add(4) // This is not allowed, so this function is not pure
     }
-     
+}
+
+fun testLocalStatesAlterable() {
     @Pure
     fun alterInnerStateClass() {
         @LocalState
         val existingArrayList = ArrayList<String>()
-        existingArrayList.remove("string") // Anything is allowed on a LocalState variable
+        existingArrayList.add("string") // Anything is allowed on a LocalState variable
     }
 }
 
-class SampleClass {
-    @Immutable
-    val immutableMap = mapOf(1 to 2, 3 to 4)
+fun testAutorecognizeSingleReturnFunctionOnValAsPure(){
+    // On function variables
+    
+    val map = mapOf(1 to 2, 3 to 4)
+    fun returnMap() = map // Autorecognized as pure
+
     @Pure
-    fun pureFunction(int: Int): Int {
-        return immutableMap[int] ?: 0
+    fun getReturnMap() = returnMap() // can call pure function
+
+    // On class properties
+    class SampleClass {
+        val map = mapOf(1 to 2, 3 to 4)
+        fun returnMap() = this.map
+
+        @Pure
+        fun getReturnMutableMap() = returnMap()
     }
-    
-    // Autorecognized as pure
-    fun returnMap() = immutableMap
-    
-    @Pure
-    fun getReturnMap() = returnMap()
-    
-    var mutableMap = mutableMapOf(1 to 2, 3 to 4)
-    fun returnMutableMap() = mutableMap
-    
-    @Readonly
-    fun getReturnMutableMap() = returnMutableMap()
-    
-    @Pure @TestExpectCompileError
-    // returnMutableMap is recognized as readonly, not pure
-    fun incorrectPureGetReturnMutableMap() = returnMutableMap() // This is not pure, because it returns a mutable map
 }
 
+fun testAutorecognizeSingleReturnFunctionOnVarAsReadonly(){
+    class SampleClass {
+        var mutableMap = mutableMapOf(1 to 2, 3 to 4)
+        fun returnMutableMap() = mutableMap
 
+        @Readonly
+        fun getReturnMutableMap() = returnMutableMap()
+
+        @Pure @TestExpectCompileError
+        // returnMutableMap is recognized as readonly, not pure
+        fun incorrectPureGetReturnMutableMap() = returnMutableMap() // This is not pure, because it returns a mutable map
+    }
+}
+
+// Apparently interfaces cannot be defined within functions, who knew?
 interface AreaCalculator {
-    @Pure
-    fun area(): Int
+    @Pure fun area(): Int
 }
 
-class Square(val width: Int) : AreaCalculator {
-    // Should be defined as pure since it overrides a pure function
-    override fun area(): Int {
-        return width * width
-    }
+fun testMarkingInterfaceMarksImplementations() {
+    // If an interface is marked as @Pure, all implementations are considered pure
+    class Square(val width: Int) : AreaCalculator {
+        override fun area(): Int = width * width  // Checked as if the function is marked with @Pure
 
-    @Pure
-    fun otherFunction(): Int {
-        return area()
+        @Pure
+        fun otherFunction(): Int = area() // Can call area() since it is considered @Pure
     }
 }
