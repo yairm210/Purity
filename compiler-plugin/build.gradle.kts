4ee092f1a79e99dc4748912f609b07533588cdc2
Yair Morgenstern <yair_morgenstern@il.yair.morgenstern.m2max16>
2025-07-22 14:37:32 +0300

    0.0.33 More more well known functions

--- compiler-plugin/build.gradle.kts
+++ compiler-plugin/build.gradle.kts
@@ -20,7 +20,7 @@ allprojects {
 }
 
 group = "io.github.yairm210"
-version = "0.0.32"
+version = "0.0.33"
 
 mavenPublishing {
     coordinates(group.toString(), "purity-compiler-plugin", version.toString())
