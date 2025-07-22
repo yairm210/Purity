c1c6f3aac5adfbd3c8279460f33e76d2a521e0ac
Yair Morgenstern <yair_morgenstern@il.yair.morgenstern.m2max16>
2025-07-22 10:15:25 +0300

    0.0.32 More well known functions

--- annotations/src/commonMain/kotlin/LocalState.kt
+++ annotations/src/commonMain/kotlin/LocalState.kt
@@ -7,4 +7,4 @@ import kotlin.annotation.AnnotationTarget.PROPERTY
  * Indicates that function variable is constructed within the function, and thus all its functions may be considered Pure.
  * This is a stronger version of [Immutable], which only allows for read-only functions to be considered Pure.
  */ 
-@Target(LOCAL_VARIABLE) public annotation class LocalState
+@Target(LOCAL_VARIABLE, PROPERTY) public annotation class LocalState
