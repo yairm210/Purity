0012202c1d01c495ed3eb4cacff4632b3afc55e6
Yair Morgenstern <yair_morgenstern@il.yair.morgenstern.m2max16>
2025-07-24 23:03:13 +0300

    0.0.37 Autorecognize Enum.valueOf(), Enum.values() functions

--- compiler-plugin/build.gradle.kts
+++ compiler-plugin/build.gradle.kts
@@ -20,7 +20,7 @@ allprojects {
 }
 
 group = "io.github.yairm210"
-version = "0.0.36"
+version = "0.0.37"
 
 mavenPublishing {
     coordinates(group.toString(), "purity-compiler-plugin", version.toString())
