fdc3760d1bbb362191efb3292d40a6a6f0e1e1f0
yairm210 <yairm210@hotmail.com>
2025-08-08 12:00:40 +0300

    0.0.51

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
 
 
 group = "io.github.yairm210"
-version = "0.0.50"
+version = "0.0.51"
 
 gradlePlugin {
     website = "https://github.com/yairm210/purity"
--- gradle-plugin/src/main/kotlin/yairm210/purity/PurityGradlePlugin.kt
+++ gradle-plugin/src/main/kotlin/yairm210/purity/PurityGradlePlugin.kt
@@ -30,7 +30,7 @@ class PurityGradlePlugin : KotlinCompilerPluginSupportPlugin {
     companion object {
         const val COMPILER_PLUGIN_GROUP_NAME = "io.github.yairm210"
         const val ARTIFACT_NAME = "purity-compiler-plugin"
-        const val VERSION_NUMBER = "0.0.50"
+        const val VERSION_NUMBER = "0.0.51"
     }
 
     private var gradleExtension : PurityConfiguration = PurityConfiguration()
