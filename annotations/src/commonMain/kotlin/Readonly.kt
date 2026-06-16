63af7ee61c8c9c0e0ada1f5cf19da2cfe4ab14c9
yairm210 <yairm210@hotmail.com>
2026-06-16 10:17:35 +0300

    chore(typo) Proof Of Humanity I guess

--- annotations/src/commonMain/kotlin/Readonly.kt
+++ annotations/src/commonMain/kotlin/Readonly.kt
@@ -5,8 +5,8 @@ import kotlin.annotation.AnnotationTarget.VALUE_PARAMETER
 
 /**
  * An annotation to indicate that this function is Readonly.
- * This means that it does not alter 
- * Functions which read from mutable state can be readonly, but functions which *write* to state cannot be.
+ * This means that it does not alter state.
+ * Functions which *read* from mutable state can be readonly, but functions which *write* to state are not.
  * 
  * The equivalent of this for external functions is WellKnownReadonlyFunctions - see https://yairm210.github.io/Purity/usage/configuration/#handling-external-classes
  */
